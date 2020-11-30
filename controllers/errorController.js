6dd4647b61d20b856722551c210964800660631a
saknarin <saknarin.css57@ubru.ac.th>
2020-11-30 12:55:37 +0700

    Better Errors and Refactoring

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,9 @@
 module.exports = (err, req, res, next) => {
 
     err.statusCode = err.statusCode || 500;
-    err.status = err.sratus || 'error'
+    err.status = err.status || 'error'
     res.status(err.statusCode).json({
-        status:err.sratus,
+        status:err.status,
         message: err.message
     });
 }
